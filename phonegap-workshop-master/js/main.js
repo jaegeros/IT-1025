883d2d38e9bf38207fb127c9c717aba1e9211e07
Clark Levan Rosario <clarkbtkbtk@gmail.com>
2016-08-04 18:52:13 -0700

    Development

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,10 +14,12 @@ var app = {
     },
 	renderHomeView: function() {
 	    var html =
+	    		"<div style='text-align: center;'>"+
 	            "<div class='header'><h1>Home</h1></div>" +
 	            "<div class='search-view'>" +
 	            "<input class='search-key'/>" +
 	            "<ul class='employee-list'></ul>" +
+	            "</div>"+
 	            "</div>"
 	    $('body').html(html);
 	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
