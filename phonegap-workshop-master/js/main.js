425f44ca35cc81ba565e8e0203e2e6b04495033a
Clark Levan Rosario <clarkbtkbtk@gmail.com>
2016-08-04 18:06:28 -0700

    Development

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -23,7 +23,7 @@ var app = {
 	
 	initialize: function() {
 	    var self = this;
-	    this.store = new LocalStorageStore(function() {
+	    this.store = new MemoryStore(function() {
 	        self.showAlert('Store Initialized', 'Info');
 	    });
 	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
